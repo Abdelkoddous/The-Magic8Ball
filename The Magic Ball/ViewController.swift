185b5661b7bb47625860fc723d790b67ab6fa085
abdelkoddouszaidi <dslpo@gmx.de>
2018-03-27 18:41:20 +0200

    Initial Commit

---
+++ The Magic Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Magic Ball
+//
+//  Created by Abdelkoddous Zaidi on 27.03.18.
+//  Copyright © 2018 Abdelkoddous Zaidi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
