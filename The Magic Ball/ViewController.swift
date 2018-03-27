14a1c862fad50c7526d6306b742be6eeb32e9a95
abdelkoddouszaidi <dslpo@gmx.de>
2018-03-27 20:14:11 +0200

    My magic ball app

--- The Magic Ball/ViewController.swift
+++ The Magic Ball/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var image_MagicBall: UIImageView!
+    
+    //create array with all magicBall picture
+    let magicBalls = ["ball1","ball2","ball3","ball4","ball5"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    @IBAction func asButton(_ sender: Any) {
+        
+        updateMagicBall()
+        
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateMagicBall()
     }
 
+    private func updateMagicBall(){
+        
+        let randomBall = Int(arc4random_uniform(UInt32(magicBalls.count)))
+        
+        image_MagicBall.image = UIImage(named: magicBalls[randomBall])
+        
+    
+    }
+    
 
 }
 
